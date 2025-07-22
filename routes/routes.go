861ee75d994af3108e65637a85ba2d66271da413
yoanesber <yoanes.berchmans89@gmail.com>
2025-07-22 22:02:37 +0700

    Enhance: Code

--- routes/routes.go
+++ routes/routes.go
@@ -10,6 +10,7 @@ import (
 	"github.com/yoanesber/go-idempotency-api/pkg/middleware/headers"
 	"github.com/yoanesber/go-idempotency-api/pkg/middleware/idempotency"
 	"github.com/yoanesber/go-idempotency-api/pkg/middleware/logging"
+	request_filter "github.com/yoanesber/go-idempotency-api/pkg/middleware/request-filter"
 	httputil "github.com/yoanesber/go-idempotency-api/pkg/util/http-util"
 )
 
@@ -24,6 +25,7 @@ func SetupRouter() *gin.Engine {
 		headers.SecurityHeaders(),
 		headers.CorsHeaders(),
 		headers.ContentType(),
+		request_filter.DetectParameterPollution(),
 		logging.RequestLogger(),
 		gzip.Gzip(gzip.DefaultCompression),
 	)
